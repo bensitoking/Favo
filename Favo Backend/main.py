8aec6490a0a71a6f8baf215875ba089a1f86bb42
Android Studio <devandroid@ort>
2025-10-20 10:17:39 -0300

    cambio de puerto para render 2

--- Favo Backend/main.py
+++ Favo Backend/main.py
@@ -604,4 +604,5 @@ async def options_handler(path: str):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
+    print(f"Starting server on port {port}")
     uvicorn.run(app, host="0.0.0.0", port=port)
